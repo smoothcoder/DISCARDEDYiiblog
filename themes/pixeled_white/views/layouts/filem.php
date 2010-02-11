3b0038f032744bc555938e0846910ca7d3af2b5f
marie <smoothcoder@discardedteenz.com>
2010-02-11 13:49:58 -0300

    build

---
+++ themes/pixeled_white/views/layouts/filem.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
+	<?php echo CHtml::cssFile(Yii::app()->theme->baseUrl.'/css/style.css'); ?>
+	<?php echo CHtml::cssFile(Yii::app()->theme->baseUrl.'/css/form.css'); ?>
+	<title><?php echo $this->pageTitle; ?></title>
+	<?php
+// javascript
+  $cs=Yii::app()->clientScript;
+  $cs->registerCoreScript('jquery');
+  $cs->registerScriptFile(Yii::app()->request->baseUrl.'/js/highslide/highslide.js', CClientScript::POS_HEAD);
+  $cs->registerScriptFile(Yii::app()->request->baseUrl.'/js/highslide/highslide_eh.js', CClientScript::POS_HEAD);
+  $cs->registerScriptFile(Yii::app()->request->baseUrl.'/js/persist.js', CClientScript::POS_HEAD);
+  $cs->registerScriptFile(Yii::app()->request->baseUrl.'/js/jquery.clipboard-2.0.1/jquery.clipboard.js', CClientScript::POS_HEAD);
+
+  $cs->registerScriptFile(Yii::app()->request->baseUrl.'/js/jquery-lightbox/jquery.lightbox.js', CClientScript::POS_HEAD);
+  $cs->registerCSSFile(Yii::app()->request->baseUrl.'/js/jquery-lightbox/css/lightbox.css');
+
+  $cs->registerLinkTag('alternate','application/rss+xml',$this->createUrl('site/postFeed'));
+  $cs->registerLinkTag('alternate','application/rss+xml',$this->createUrl('site/commentFeed'));
+
+  $params = array(
+		'BASEURL'=>Yii::app()->request->baseUrl,
+                'HTTPHOST'=>$_SERVER['HTTP_HOST']
+		  );
+  $script = 'var PARAMS = eval('.CJavaScript::jsonEncode($params).');';
+  $cs->registerScript('widget-oc1', $script, CClientScript::POS_BEGIN);
+  $script = implode('',file(Yii::app()->basePath.'/../js/widget-oc.min.js'));
+  $cs->registerScript('widget-oc2', $script, CClientScript::POS_READY);
+  $script = 'hs.graphicsDir = PARAMS.BASEURL+\'/js/highslide/graphics/\';'."\n";
+  $script .= 'hs.outlineType = \'rounded-white\';'."\n";
+  $script .= 'hs.showCredits = false;';
+  $cs->registerScript('hislide-end', $script, CClientScript::POS_BEGIN);
+  $script = 'addHighSlideAttribute();';
+  $cs->registerScript('hislide-end', $script, CClientScript::POS_END);
+// css
+  $cs->registerCSSFile(Yii::app()->request->baseUrl.'/js/highslide/highslide.css');
+
+?>
+
+<script type="text/javascript">
+$(document).ready(function(){
+
+	$(".lightbox").lightbox({
+		fitToScreen: true,
+		fileLoadingImage : PARAMS.BASEURL+'/js/jquery-lightbox/images/loading.gif',
+		fileBottomNavCloseImage : PARAMS.BASEURL+'/js/jquery-lightbox/images/closelabel.gif',
+		imageClickClose: false
+	});
+});
+</script>
+
+
+<?php
+Yii::app()->clientScript->registerScript(
+   'myHideEffect2',
+   '$(".message").animate({opacity: 1.0}, 7000).fadeOut("slow");',
+   CClientScript::POS_READY
+);
+?>
+</head>
+
+<body>
+	<div id="page">
+
+
+		<div id="content">
+			<div id="contentwrapper">
+				<?php echo $content; ?>
+			</div><!-- contentwrapper -->
+			<div class="cleared"></div>
+		</div><!-- content -->
+
+		<div class="cleared"></div>
+
+		<div id="footer">
+
+		</div><!-- footer -->
+
+	</div><!-- page -->
+</body>
+
+</html>
